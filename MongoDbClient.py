c97ebe86e48d62fd82dd40e9a9516cadf4090faa
praveen-mg <praveen_mg17@yahoo.co.in>
2016-07-05 17:35:18 +0800

    Unique Users

--- MongoDbClient.py
+++ MongoDbClient.py
@@ -8,7 +8,14 @@ def find():
     cities = db.city.find({ "name": "PIE"})
     for a in cities:
         pprint.pprint(a)
-
+    #user_cboothroyd = db.city.find({"created.user" : "cboothroyd"})
+    
+    #for a in user_cboothroyd:
+        #pprint.pprint(a)
+        
+    #print "Number contributed by cboothroyd", user.count()
+    print "Total number of distinct Users:",len(db.city.distinct("created.user"))
+    #print ""db.city.find({"type":"node"}).count()
 
 if __name__ == '__main__':
     find()
